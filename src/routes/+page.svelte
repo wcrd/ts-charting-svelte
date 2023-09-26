6dff139bdfe20cc65710676e20c1b872dbf0ed80
wcrd <wcrd@pop-os.localdomain>
2023-09-26 14:37:28 -0600

    Add main page links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container m-2">
+    <h1 class="text-xl">TS data visualtisation testing</h1>
+    <div>
+        <p>This site contains the following sections:</p>
+        <ul class="list-disc list-inside">
+            <li>d3 graph testing: <a href="/basic-charts" class="text-purple-600">link</a> </li>
+            <li>Custom d3 graph component testing: <a href="/custom-components" class="text-purple-600">link</a> </li>
+            <li>Final TS Svelte D3 chart component: <a href="/final-component-chart" class="text-purple-600">link</a> </li>
+        </ul>
+    </div>
+</div>
